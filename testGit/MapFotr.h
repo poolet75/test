6bc1cecc94aaf25aa03fc5f5568af14942ebe635
Анатолий Балабанов <poolet@iMac-Anatolij.local>
2014-01-30 15:53:52 +0600

    Commit to new Branch

---
+++ testGit/MapFotr.h
@@ -0,0 +1,13 @@
+//
+//  MapFotr.h
+//  testGit
+//
+//  Created by Анатолий Балабанов on 30.01.14.
+//  Copyright (c) 2014 Анатолий Балабанов. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapFotr : NSObject
+
+@end
---
+++ testGit/MapFotr.m
@@ -0,0 +1,13 @@
+//
+//  MapFotr.m
+//  testGit
+//
+//  Created by Анатолий Балабанов on 30.01.14.
+//  Copyright (c) 2014 Анатолий Балабанов. All rights reserved.
+//
+
+#import "MapFotr.h"
+
+@implementation MapFotr
+
+@end
